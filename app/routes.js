65e4e0f5ef63f04bd58cf5e2d35daf96e4e96858
Noah Scholfield <contact@noahscholfield.com>
2017-08-07 16:25:16 -0400

    Add Mailchimp endpoint for signup sheet

--- app/routes.js
+++ app/routes.js
@@ -18,6 +18,28 @@ module.exports = function(app, passport) {
     polls.changeCurrent(req, res);
   });
 
+  app.post('/api/mailchimp/', function(req, res) {
+    var newSubscriber = req.body;
+    var response = {success: false};
+    request.post('https://us15.api.mailchimp.com/3.0/lists/45f7988056/members/', {
+      auth: {
+        'user': 'username',
+        'pass': process.env.MAILCHIMP_KEY
+      },
+      json: newSubscriber
+    }, function (error, response, body) {
+      if(!error && body.status == 'subscribed' && body.email_address) {
+        response.success = true;
+        response.message = `${body.email_address} was subscribed!`;
+      } else if(body.status === 400 && body.title === 'Member Exists') {
+        response.message = `Looks like ${newSubscriber.email_address} is already subscribed.`;
+      } else {
+        response.message = 'Sorry, that didn\'t work. Can you try again?';
+      }
+    });
+    res.json(response);
+  });
+
   app.get('/register/', function(req, res) {
     if(req.isAuthenticated()) {
       res.redirect('/');
